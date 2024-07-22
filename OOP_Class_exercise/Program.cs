72fab2d8b527f9ffea02df52d24d8d94d1921200
whs1997 <whs1997@naver.com>
2024-07-22 16:37:21 +0900

    exercise 1
    객체지향과 클래스 과제 1

---
+++ OOP_Class_exercise/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace OOP_Class_exercise
+{
+    internal class Program
+    {
+        class Character
+        {
+            // 필드. 개체가 가지는 데이터
+            int level;
+            int hp;
+            int moveSpeed;
+            int atk;
+
+            // 메서드, 개체가 가지는 동작
+            public void MoveFront() // 전진
+            {
+                
+            }
+
+            public void MoveBackward() // 후진
+            {
+
+            }
+
+            public void TurnLeft() // 좌회전(90도)
+            {
+
+            }
+
+            public void TurnRight() // 우회전(90도)
+            {
+
+            }
+
+            public void Attack() // 공격
+            {
+
+            }
+
+            public void GetDamage() // 피격
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
